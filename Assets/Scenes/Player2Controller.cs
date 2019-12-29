90c2700fee073f0ada87ba331471008ee63912ce
Kevin Ge <gekevin2000@gmail.com>
2019-12-29 17:46:15 -0500

    custom

---
+++ Assets/Scenes/Player2Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        Debug.Log("Move: " + move * 5);
+        anim.SetFloat("Speed", move * 5);
+    }
+}
